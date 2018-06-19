61d98a119ab2938d1074dd60100c48798ce76151
hsin78318 <hsin78318@gmail.com>
2018-06-19 18:20:18 +0800

    Delete the testViewController and launch the latest version

--- One-image Viewer/ViewController.swift
+++ One-image Viewer/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TestViewController.swift
 //  One-image Viewer
 //
 //  Created by Chen Hsin on 2018/6/19.
@@ -8,24 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController, UINavigationControllerDelegate, UIScrollViewDelegate, UIImagePickerControllerDelegate {
-
+class ViewController: UIViewController, UIScrollViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     @IBOutlet weak var scrollView: UIScrollView!
-    var imageView = UIImageView()
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var changePhotoButton: UIButton!
+    let fullScreenSize = UIScreen.main.bounds.size
     
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.delegate = self
-        imageView.frame = CGRect(x: 0, y: 0, width: scrollView.frame.size.width, height: scrollView.frame.size.height)
-        imageView.image = UIImage(named: "icon_photo")
-//        imageView.isUserInteractionEnabled = true
-        scrollView.addSubview(imageView)
+        self.scrollView.minimumZoomScale = 1.0
+        self.scrollView.maximumZoomScale = 2.0
         
+//        imageView.image = UIImage(named: "icon_photo")
+//        imageView.center = CGPoint(
+//            x: fullScreenSize.width * 0.5,
+//            y: fullScreenSize.height * 0.5,
+//
+//            self.view.addSubview(imageView)
+//        )
+       
     }
     
-    
-    @IBAction func selectPictureButton(_ sender: Any) {
+    @IBAction func changePhotoButton(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.sourceType = .photoLibrary
@@ -33,94 +39,24 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIScroll
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
-        
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
-        
+        scrollView.contentSize = CGSize(width:2000, height:2000)
         imageView.image = image
-        imageView.contentMode = UIViewContentMode.center
-        
-        imageView.frame = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
-        
-        scrollView.contentSize = image.size
-        
-        let scrollViewFrame = scrollView.frame
-        let scaleWidth = scrollViewFrame.size.width / scrollView.contentSize.width
-        let scaleHeight = scrollViewFrame.size.height / scrollView.contentSize.height
-       
-        let minScale = min(scaleHeight, scaleWidth)
-        scrollView.minimumZoomScale = minScale
-        scrollView.maximumZoomScale = 1
-        scrollView.zoomScale = minScale
-        
-        centerScrollViewContents()
         
         picker.dismiss(animated: true, completion: nil)
         
-    }
-    
-    
-    func setZoomScale() {
-        let imageViewSize = imageView.bounds.size
-        let scrollViewSize = scrollView.bounds.size
-        let widthScale = scrollViewSize.width / imageViewSize.width
-        let heightScale = scrollViewSize.height / imageViewSize.height
-        
-        scrollView.minimumZoomScale = min(widthScale, heightScale)
-        scrollView.zoomScale = 1.0
-    }
-    
-    
-    func centerScrollViewContents() {
-        let boundsSize = scrollView.bounds.size
-        var contentFrame = imageView.frame
-        
-        if contentFrame.size.width < boundsSize.width {
-            contentFrame.origin.x = (boundsSize.width - contentFrame.size.width) / 2
-        }
-        else {
-            contentFrame.origin.x = 0
-        }
-        
-        if contentFrame.size.height < boundsSize.height {
-            contentFrame.origin.y = (boundsSize.height - contentFrame.size.height) / 2
-            
-        }
-        else {
-            contentFrame.origin.y = 0
-        }
-        
-        imageView.frame = contentFrame
-        
-    }
-    
-    
-    
-    func scrollViewDidZoom( scrollView: UIScrollView) {
-        centerScrollViewContents()
     }
     
     func viewForZoominInScrollview(scrollView: UIScrollView) -> UIView? {
         return imageView
     }
     
-//    func imagePickerControllerDidCancel(_ Picker: UIImagePickerController) {
-//        dismiss(animated: true, completion: nil)
-//    }
-    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.imageView
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    
-    
-
-
-
-    
-    
-    
-    
-    
 }
-
