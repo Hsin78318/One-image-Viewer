4ccd8966d312420c28f201cd6f86b93a4afccd83
hsin78318 <hsin78318@gmail.com>
2018-06-19 16:03:10 +0800

    The autolayout of basic screen is done

--- One-image Viewer/TestViewController.swift
+++ One-image Viewer/TestViewController.swift
@@ -14,26 +14,34 @@ class TestViewController: UIViewController, UIScrollViewDelegate,UINavigationCon
     
     @IBOutlet weak var imageView: UIImageView!
     
+    
+    @IBOutlet weak var changePhotoButton: UIButton!
+    
+    
     @IBAction func changePhotoButton(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
-        
+        changePhotoButton.setTitleColor(UIColor.white, for: .normal)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        scrollView.delegate = self
         self.scrollView.minimumZoomScale = 1.0
         self.scrollView.maximumZoomScale = 2.0
+        
+       
+        
+        
     }
     
     
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
-        
+        scrollView.contentSize = CGSize(width:2000, height:2000)
         imageView.image = image
         
         picker.dismiss(animated: true, completion: nil)
