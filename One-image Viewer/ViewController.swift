ba60d0c7688948b56ed2feb4494090df973405a2
hsin78318 <hsin78318@gmail.com>
2018-06-19 13:02:57 +0800

    Fix the feature of SelectPicture from using UIImageView to UIScrollView

--- One-image Viewer/ViewController.swift
+++ One-image Viewer/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIScrollViewDelegate, UIImagePickerControllerDelegate {
 
+
+    @IBOutlet weak var scrollView: UIScrollView!
     
-    @IBOutlet weak var imageView: UIImageView!
-    
+    var imageView = UIImageView()
     
     @IBAction func selectPictureButton(_ sender: Any) {
         let controller = UIImagePickerController()
@@ -26,15 +27,65 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
-        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-        imageView.image = selectedImage
-        dismiss(animated: true, completion: nil)
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
+        imageView.contentMode = UIViewContentMode.center
+        imageView.frame = CGRect(x: 0, y: 0, width: scrollView.frame.size.width, height: scrollView.frame.size.height)
+        
+        scrollView.contentSize = image.size
+        
+        let scrollViewFrame = scrollView.frame
+        let scaleWidth = scrollViewFrame.size.width / scrollView.contentSize.width
+        let scaleHeight = scrollViewFrame.size.height / scrollView.contentSize.height
+        let minScale = min(scaleHeight, scaleWidth)
+        scrollView.minimumZoomScale = minScale
+        scrollView.maximumZoomScale = 1
+        scrollView.zoomScale = minScale
+        centerScrollViewContents()
+        picker.dismiss(animated: true, completion: nil)
+
     }
     
+    func centerScrollViewContents() {
+        let boundsSize = scrollView.bounds.size
+        var contentFrame = imageView.frame
+       
+        if contentFrame.size.width < boundsSize.width {
+            contentFrame.origin.x = (boundsSize.width - contentFrame.size.width) / 2
+        }
+        else {
+            contentFrame.origin.x = 0
+        }
+        
+        if contentFrame.size.height < boundsSize.height {
+            contentFrame.origin.y = (boundsSize.height - contentFrame.size.height) / 2
+            
+        }
+        else {
+             contentFrame.origin.y = 0
+        }
+        
+        imageView.frame = contentFrame
+        
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        centerScrollViewContents()
+    }
+    
+    func viewForZoominInScrollview(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        scrollView.delegate = self
+        imageView.frame = CGRect(x: 0, y: 0, width: scrollView.frame.size.width, height: scrollView.frame.size.height)
+        imageView.image = UIImage(named: "imageView")
+        imageView.isUserInteractionEnabled = true
+      
+        scrollView.addSubview(imageView)
+        
     }
 
     override func didReceiveMemoryWarning() {
